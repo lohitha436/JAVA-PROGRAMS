eb1578c4ba3b173ad45738799befc12017669aac
DUPPANAPUDI LOHITHA <23wh1a05g4@bvrithyderabad.edu.in>
2024-08-22 12:38:31 +0530

    Add files via upload

--- OverRiding.java
+++ OverRiding.java
@@ -1,5 +1,19 @@
 package lohitha;
-
+class Animal{
+	public void display()
+	{
+		System.out.println("Tiger");
+	}
+}
+class Lion extends Animal{
+	public void display()
+	{
+		System.out.println("Rabbit");
+	}
+}
 public class OverRiding {
-
+public static void main(String[] args) {
+	Lion obj=new Lion();
+	obj.display();
+	}
 }
