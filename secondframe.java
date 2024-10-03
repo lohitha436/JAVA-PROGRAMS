cb03935123b5516e4ea2cb840efcfdc690ff2220
DUPPANAPUDI LOHITHA <23wh1a05g4@bvrithyderabad.edu.in>
2024-10-03 12:24:21 +0530

    Create secondframe.java

---
+++ secondframe.java
@@ -0,0 +1,19 @@
+import java.awt.Button;
+import java.awt.Frame;
+
+public class secondframe {
+	static class First2{
+		First2(){
+		Frame f=new Frame();
+		Button b=new Button("BVRIT HYDERABAD");
+		b.setBounds(30,50,80,30);
+		f.add(b);
+		f.setSize(300,300);
+		f.setLayout(null);
+		f.setVisible(true);
+		}
+		public static void main(String[] args){
+			First2 f=new First2();
+		}
+		}
+}
