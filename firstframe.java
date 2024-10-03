6b826a9ba109745f910401137cf12c92496fd28b
DUPPANAPUDI LOHITHA <23wh1a05g4@bvrithyderabad.edu.in>
2024-10-03 12:23:45 +0530

    Create firstframe.java

---
+++ firstframe.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+public class firstframe {
+	public static void main(String[] args) {
+		Frame f = new Frame ("My First Frame");
+		f.setSize(400,300);
+		f.setVisible(true);
+	}
+}
